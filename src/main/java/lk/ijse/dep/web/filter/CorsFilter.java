093f7d3a018b02d52df5e11c6da9110b5bf970c1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-01-13 01:02:30 +0530

    Add authorization header to CorsFilter

--- src/main/java/lk/ijse/dep/web/filter/CorsFilter.java
+++ src/main/java/lk/ijse/dep/web/filter/CorsFilter.java
@@ -13,10 +13,9 @@ public class CorsFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        System.out.println("incoming");
         res.setHeader("Access-Control-Allow-Origin","http://localhost:4200");
         res.setHeader("Access-Control-Allow-Methods","GET, PUT, POST, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers","Content-Type");
+        res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization");
         chain.doFilter(req,res);
     }
 }
